41dde4b85b2d519c1418e439e9830b638a373745
bochan <himanshubora972@gmail.com>
2025-06-03 13:21:36 +0530

    day02

---
+++ 02_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs ,not browser
+
+
+let name = "bocie"
+let age = 18
+let isloggedIn = false
+let state;//undefined
+
+//primitive data types
+// number = 2 to power 53
+//bigint (for large number)
+//string =>""
+//boolean => true/false
+//null => standalone value (representing empty)
+// undefined => value not assigned 
+// symbol => unique
+
+
+//object
+
+ console.log(typeof null) // type of null is object
