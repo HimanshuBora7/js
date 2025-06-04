d4ab2aebdc12aab466c3397b24e69c16f2ceaacf
bochan <himanshubora972@gmail.com>
2025-06-04 18:37:18 +0530

    day02a

--- 03_conversion.js
+++ 03_conversion.js
@@ -1,16 +1,16 @@
 let score = "33"
-console.log(score)
-console.log(typeof score)
+//console.log(score)
+//console.log(typeof score)
 
 
 let ValueInNumber = Number (score)
 
-console.log(typeof ValueInNumber)
+//console.log(typeof ValueInNumber)
 
 
 let score1 = "3abc32"
 let ValueInScore1 = Number(score1)
-console.log(ValueInScore1) 
+//console.log(ValueInScore1) 
 
 //"33"=> 33 if variable holds a valid number and conversion is possible them we get the right conversion 
 //but it the value to be converted consists
@@ -22,8 +22,18 @@ console.log(ValueInScore1)
 let isloggedIn = null
 
 let booleanisLoggedIn = Boolean(isloggedIn)
-console.log(booleanisLoggedIn)
+//console.log(booleanisLoggedIn)
 
+// *********************** Operations ***********************
 
+ let value =3
+ let negValue = -value
+ // console.log(negValue)
+
+// console.log(2**2)//2**2 means 2 raised to the power of 2
+  
+console.log("1"+2+2); //we expect 14 but we get 122 
+
+console.log(1+2+"2") // but for this 32 will be the output 
 
 
