6b032060483a09c10368b4408eb4be326b94a42c
bochan <himanshubora972@gmail.com>
2025-06-09 21:30:05 +0530

    strings

--- o5_string.js
+++ o5_string.js
@@ -1,7 +1,10 @@
 const name = "bochie"
 const repoCount = 50
 
-//console.log(name + repoCount)
+//console.log(name + repoCount) // this syntax is an outdated way for concatination of strings 
+
+//instead now we use backticks
+//string interpolation (or variable interpolation, variable substitution, or variable expansion) is the process of evaluating a string literal containing one or more placeholders, yielding a result in which the placeholders are replaced with their corresponding values. It is a form of simple template processing[1] or, in formal terms, a form of quasi-quotation (or logic substitution interpretation). The placeholder may be a variable name, or in some languages an arbitrary expression, in either case evaluated in the current context.
 
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
