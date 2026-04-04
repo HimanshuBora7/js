ecda63fe02715d2dd32408c5c5aefe2631da2629
himanshubora7 <himanshubora972@gmail.com>
2026-04-04 12:18:46 +0530

    revising js

---
+++ lec1/dataType.js
@@ -0,0 +1,20 @@
+// console.log("commenting")
+
+// string
+
+let myName = "lucy";
+
+console.log(typeof myName);
+
+// symbol ~ to make things unique
+
+let sm1 = Symbol();
+let sm2 = Symbol(4);
+console.log(typeof sm1);
+console.log("sm1 ", sm1);
+console.log("sm2 ", sm2);
+let sm3 = 4;
+let sm4 = "four";
+
+console.log(sm1 == sm2);
+console.log(sm3 == sm4);
---
+++ lec1/printing.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+process.stdout.write("chai\n");
+
+let obj1 = {
+  name: "bochie",
+  city: "kasukabe",
+};
+console.table(obj1); //output in table format
+
+console.warn(obj1); //colurify the output to make it stand out
