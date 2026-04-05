7f31b38cad5e8f84c54af5627668b6d3486c6852
himanshubora7 <himanshubora972@gmail.com>
2026-04-05 10:00:05 +0530

    nonPrimitives

---
+++ lec2/nonPrimitive.js
@@ -0,0 +1,21 @@
+let username = {
+  "first Name": "ichigai",
+  lastName: "sukomot",
+};
+
+console.log(username);
+console.log(typeof username);
+console.log(username["first Name"]);
+
+// dates
+let today = new Date();
+console.log(today.getMilliseconds());
+console.log(today.getDate());
+console.log(typeof today);
+//array
+let num = [1, 2, 3, 4, "five"];
+
+console.log(typeof num);
+console.log(num);
+console.log(num[0]);
+console.log(num.length);
