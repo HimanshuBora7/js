3567dceef1d654986a5b5f830aec4b81ed7135ac
himanshubora7 <himanshubora972@gmail.com>
2026-04-09 10:46:26 +0530

    oops in js

--- part6/oop-master.js
+++ part6/oop-master.js
@@ -68,3 +68,107 @@ class Tank extends Vehicle {
 let myCar = new Tank("drdo", "zoravar");
 console.log(myCar.start());
 console.log(myCar.drive());
+
+// Encapsulation ~ in simple terms restricting the direct access to object data
+class Bank {
+  #balance = 0; // by adding hash now this variable will be not be directly accessed
+  // if we want acess this we have to create a method
+
+  deposit(amount) {
+    this.#balance += amount;
+    // return this.#balance;
+    return this.getBalance();
+  }
+  getBalance() {
+    return `$ ${this.#balance}`;
+  }
+}
+
+let account = new Bank();
+console.log(account.getBalance());
+console.log(account.deposit(100));
+console.log(account.getBalance());
+
+// abstraction ~ hiding the complex implementation details
+
+class CoffeMachine {
+  start() {
+    //call db
+    //filter
+    return ` starting the machine....`;
+  }
+  brewCofee() {
+    //complex calculation
+    return `Brewing coffee`;
+  }
+  pressStartButton() {
+    let msg1 = this.start();
+
+    let msg2 = this.brewCofee();
+
+    return ` ${msg1} , ${msg2}`;
+  }
+}
+let myMachine = new CoffeMachine();
+console.log(myMachine.start());
+console.log(myMachine.brewCofee());
+console.log(myMachine.pressStartButton());
+
+//polymorphism ~ many form
+class Bird {
+  fly() {
+    return `flying......`;
+  }
+}
+
+class Penguin extends Bird {
+  fly() {
+    return `penguin can't fly`;
+  }
+}
+
+let bird = new Bird();
+let penguin = new Penguin();
+
+console.log(bird.fly());
+console.log(penguin.fly());
+
+// static method
+// static method in js can oonly be called by class itself only
+
+class Calculator {
+  static add(a, b) {
+    return a + b;
+  }
+}
+
+// let miniCalc = new Calculator();
+
+// console.log(miniCalc.add(3, 2));
+
+console.log(Calculator.add(3, 2));
+
+// Getters and setters
+
+class Employee {
+  #salary;
+  constructor(name, salary) {
+    this.name = name;
+    this.#salary = salary;
+  }
+
+  get salary() {
+    return `you are not allowed to see salary `;
+  }
+
+  set salary(value) {
+    if (value < 0) {
+      console.error("Invalid salary");
+    }
+    this._salary = value;
+  }
+}
+
+let emp1 = new Employee("john", -50000);
+console.log(emp1.salary); // u can see here with salary method we have not used () becoz we are using get and set keyword
+emp1.salary = -3000;
